cb1c907c72c3cc779e5ffe100518ba7d3736df5c
Niji Ano <fab-bertheol@hotmail.fr>
2023-05-19 11:17:24 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -14,13 +14,14 @@ import datetime
 import glob
 import zipfile
 from colorama import init, Fore, Style
+
 init()
 
 
 def sep_sys():
-    if sys.platform == 'win32':
-        return r'\\'
-    return '/'
+    if sys.platform == "win32":
+        return r"\\"
+    return "/"
 
 
 class Logger:
@@ -31,34 +32,32 @@ class Logger:
     """
 
     def __init__(self):
-        self.log_nms = 'logger'
-        self.logs_dir = '.' + sep_sys() + '.logs'
+        self.log_nms = "logger"
+        self.logs_dir = "." + sep_sys() + ".logs"
         self.level = 1
         self.save = True
 
-        if '--lv2' in sys.argv:
+        if "--lv2" in sys.argv:
             self.level = 2
 
-        elif '--lv3' in sys.argv:
+        elif "--lv3" in sys.argv:
             self.level = 3
 
-        if os.name == 'nt':
-            os.system('')
-
+        if os.name == "nt":
+            os.system("")
 
     def index_file(self):
-        """Indexer module
-        """
+        """Indexer module"""
         if os.path.isdir(self.logs_dir):
             try:
                 cwd = os.getcwd()
                 os.chdir(self.logs_dir + sep_sys())
-                now = str(datetime.datetime.now())[:-7].split(' ')[0]
-                files_list = glob.glob('*.log')
+                now = str(datetime.datetime.now())[:-7].split(" ")[0]
+                files_list = glob.glob("*.log")
                 for file in files_list:
                     if now not in file:
-                        fl_dt = file.split('_')[-1][:-4]
-                        with zipfile.ZipFile(fl_dt + '_logs.zip', 'a') as log_zip:
+                        fl_dt = file.split("_")[-1][:-4]
+                        with zipfile.ZipFile(fl_dt + "_logs.zip", "a") as log_zip:
                             log_zip.write(file)
 
                         os.remove(file)
@@ -81,9 +80,17 @@ class Logger:
         if not os.path.isdir(self.logs_dir):
             os.mkdir(self.logs_dir)
 
-        log_file = open(self.logs_dir + sep_sys() + self.log_nms + '_' + str(
-            datetime.datetime.now())[:-7].split(' ')[0] + ".log", mode="a+", encoding="utf-8")
-        log_file.write(line + '\n')
+        log_file = open(
+            self.logs_dir
+            + sep_sys()
+            + self.log_nms
+            + "_"
+            + str(datetime.datetime.now())[:-7].split(" ")[0]
+            + ".log",
+            mode="a+",
+            encoding="utf-8",
+        )
+        log_file.write(line + "\n")
         log_file.close()
 
     def params(self, key):
@@ -91,12 +98,12 @@ class Logger:
         - var list: HEADER, INFO, OK, WARNING, FAIL, END, BOLD, UNDERLINE
         """
         values = {
-            'INFO': Style.BRIGHT + Fore.CYAN,
-            'OK': Style.BRIGHT + Fore.GREEN,
-            'WARNING': Style.BRIGHT + Fore.YELLOW,
-            'FAIL': Style.BRIGHT + Fore.RED,
-            'DEFAULT': Style.BRIGHT + Fore.WHITE,
-            'END': Style.RESET_ALL,
+            "INFO": Style.BRIGHT + Fore.CYAN,
+            "OK": Style.BRIGHT + Fore.GREEN,
+            "WARNING": Style.BRIGHT + Fore.YELLOW,
+            "FAIL": Style.BRIGHT + Fore.RED,
+            "DEFAULT": Style.BRIGHT + Fore.WHITE,
+            "END": Style.RESET_ALL,
         }
         return values.get(key, Style.RESET_ALL)
 
@@ -108,24 +115,37 @@ class Logger:
         text [str] -- text to display
         lv [int] -- log level: info(1) debug(2) trace(3)
         """
-        if typ == 'info':
-            color = self.params('INFO')
-        elif typ == 'warning':
-            color = self.params('WARNING')
-        elif typ == 'error':
-            color = self.params('FAIL')
+        if typ == "info":
+            color = self.params("INFO")
+        elif typ == "warning":
+            color = self.params("WARNING")
+        elif typ == "error":
+            color = self.params("FAIL")
         else:
-            color = self.params('OK')
+            color = self.params("OK")
 
-        return_txt = '[' + str(datetime.datetime.now())[:-3] + '] '
-        return_txt += '[' + color + typ.capitalize().ljust(7) + self.params('END') + '] '
-        return_txt += '[' + self.params('DEFAULT') + self.log_nms.ljust(10) + self.params('END') + '] '
-        return_txt += color + str(text) + self.params('END')
+        return_txt = "[" + str(datetime.datetime.now())[:-3] + "] "
+        return_txt += (
+            "[" + color + typ.capitalize().ljust(7) + self.params("END") + "] "
+        )
+        return_txt += (
+            "["
+            + self.params("DEFAULT")
+            + self.log_nms.ljust(10)
+            + self.params("END")
+            + "] "
+        )
+        return_txt += color + str(text) + self.params("END")
 
         if self.level >= lv:
             print(return_txt)
 
         if self.save:
-            self.save_on_file('[' + str(
-                datetime.datetime.now())[:-3] + '] [' + typ.capitalize().ljust(7) + '] ' + str(
-                text))
+            self.save_on_file(
+                "["
+                + str(datetime.datetime.now())[:-3]
+                + "] ["
+                + typ.capitalize().ljust(7)
+                + "] "
+                + str(text)
+            )
